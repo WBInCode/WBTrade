9342f6d54188f8f0be64cdd797f7996c8b69aac0
Jakub Sosnowski <technical-support-junior1@wb-partners.pl>
2025-12-19 10:05:44 +0100

    feat: Add PayU payment provider integration

--- apps/api/src/providers/payment/index.ts
+++ apps/api/src/providers/payment/index.ts
@@ -4,6 +4,6 @@
 
 export * from './payment-provider.interface';
 export * from './przelewy24.provider';
+export * from './payu.provider';
 // export * from './stripe.provider';
-// export * from './payu.provider';
 // export * from './tpay.provider';
