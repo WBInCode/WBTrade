705a91a16adb4badd8b6eb4239565d601ea20d20
Jakub Maziarz <technical-support-junior@wb-partners.pl>
2026-01-15 10:18:56 +0100

    Usuń zakomentowany przycisk Promocje z nagłówka

--- apps/web/src/components/Header.tsx
+++ apps/web/src/components/Header.tsx
@@ -265,20 +265,6 @@ function HeaderContent() {
                 </svg>
                 Kategorie
               </button>
-              {/* Deals Link - Highlighted */}
-              <Link
-                href="/deals"
-                className={`flex items-center gap-1.5 px-4 py-2 text-sm font-bold rounded-lg whitespace-nowrap transition-colors ${
-                  pathname === '/deals'
-                    ? 'text-primary-500 bg-white'
-                    : 'text-white hover:bg-primary-600'
-                }`}
-              >
-                <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
-                  <path fillRule="evenodd" d="M5 2a2 2 0 00-2 2v14l3.5-2 3.5 2 3.5-2 3.5 2V4a2 2 0 00-2-2H5zm2.5 3a1.5 1.5 0 100 3 1.5 1.5 0 000-3zm6.207.293a1 1 0 00-1.414 0l-6 6a1 1 0 101.414 1.414l6-6a1 1 0 000-1.414zM12.5 10a1.5 1.5 0 100 3 1.5 1.5 0 000-3z" clipRule="evenodd" />
-                </svg>
-                Promocje
-              </Link>
               {categories.map((category) => {
                 const isActive = isOnProductsPage && currentMainCategory?.slug === category.slug;
                 return (
